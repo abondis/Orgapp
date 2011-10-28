88ac8a86ce7a905ab7a5bdca313bdb2c4b662dbb
Aurélien Bondis <aurelien.bondis@gmail.com>
2011-10-28 13:47:35 -0400

    fix routes problems. pass files list to template

--- python/wiki.py
+++ python/wiki.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import os
 from bottle import route, run, static_file
 from bottle import view, template
 from bottle import get, post, request, error
@@ -9,19 +10,19 @@ def home(page='home'):
     return dict(content='Hello, how are you?',
                 page=page)
 
-@route('/wiki')
 @route('/wiki/')
 @route('/wiki/:page#.+#')
-@view('wiki')
-def wiki(page='index'):
+@view('block_content')
+def wiki(page=''):
+  files = os.listdir(os.path.dirname('./wiki/%s' % page))
+  if not page: page = 'index' or files[0]
   input_str = static_file(page, root='./wiki').output.read().decode().replace('\n','<br/>')
   try:
     import docutils
   except Exception as e:
-    return dict(page=page,content=input_str)
+    return dict(page=page,content=input_str, files=files)
   else:
-    return dict(page=page+"_docutils",content=input_str)
-  return dict(page=page,content=input_str)
+    return dict(page=page+"_docutils",content=input_str, files=files)
 
 @route('/static/:filename#.+#')
 def server_static(filename):
@@ -33,3 +34,4 @@ def error404(error):
 
 
 run(host='localhost', port=8080)
+#run(host='172.16.0.163', port=8080)
