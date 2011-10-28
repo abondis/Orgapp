6e182e54c510ac5ef4bc4edebb05da4f6e24088c
Aurélien Bondis <aurelien.bondis@gmail.com>
2011-10-28 17:01:57 -0400

    serving wiki files work. need to fix links/path

--- python/wiki.py
+++ python/wiki.py
@@ -14,15 +14,29 @@ def home(page='home'):
 @route('/wiki/:page#.+#')
 @view('wiki_2col')
 def wiki(page=''):
-  files = os.listdir(os.path.dirname('./wiki/%s' % page))
-  if not page: page = 'index' or files[0]
+  fullpath = './wiki/%s' % page
+  if not os.path.isfile(fullpath):
+    if os.path.isdir(fullpath):
+      files = os.listdir(fullpath)
+      dirname = fullpath
+      if os.path.exists(fullpath+'index'): page = page + 'index'
+      else: page = page +'/'+ files[0]
+    else:
+      dirname = '/wiki'
+  else: 
+    files = os.listdir(os.path.dirname(fullpath))
+    dirname = os.path.dirname(fullpath)
+  print('='*10)
+  print(page)
+  print('='*10)
+  if not page: page = 'index' 
   input_str = static_file(page, root='./wiki').output.read().decode().replace('\n','<br/>')
   try:
     import docutils
   except Exception as e:
-    return dict(page=page,content=input_str, files=files)
+    return dict(page=page,content=input_str, files=files, path=dirname)
   else:
-    return dict(page=page+"_docutils",content=input_str, files=files)
+    return dict(page=page+"_docutils",content=input_str, files=files, path=dirname)
 
 @route('/static/:filename#.+#')
 def server_static(filename):
@@ -32,6 +46,7 @@ def server_static(filename):
 def error404(error):
     return 'you might be looking for something you won\'t get'
 
-
-run(host='localhost', port=8080)
+import bottle
+bottle.debug(True)
+run(host='localhost', port=8080,reloader=True)
 #run(host='172.16.0.163', port=8080)
