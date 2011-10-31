26c815868e4ffaedfe6b03f08dea8bda609791c2
Aurelien Bondis <aurelien.bondis@gmail.com>
2011-10-30 22:14:58 -0400

    wiki+folder name+first line of the doc showing

--- python/views/wiki_2col.tpl
+++ python/views/wiki_2col.tpl
@@ -1,9 +1,16 @@
 %def leftblock():
 <div class='threecol'>
+%import os
 %if not path.endswith('/'): slash = '/'
 %else: slash = ''
   %for p in files:
-     <a href=/{{path}}{{slash}}{{p}}>{{p}}</a><br/>
+     %if os.path.isdir(path+slash+p):
+       %(pname,slug) = p2t[path+slash+p]
+       %pname = p
+     %else:
+       %(pname,slug) = p2t[path+slash+p]
+     %end
+     <a href='/{{slug}}'>{{pname}}</a><br/>
   %end
 </div>
 %end
--- python/wiki.py
+++ python/wiki.py
@@ -26,32 +26,60 @@ def home(page='home'):
 def render_textile(raw):
   return textile.textile(raw)
 
+def wiki_url():
+  """
+    populates dic path2title and title2path.
+    path2title: gives a set (title, titlePath) for every path in the wiki
+    title2path: gives a set (fileName, filePath) for every niceURL
+  """
+  title2path = {}
+  path2title = {}
+  for i in os.walk('wiki'):
+    for j in i[2]:
+      filePath = str.strip(i[0]+'/'+j, '\n')
+      fileName = str.strip(j, '\n')
+      f = open(filePath, 'r')
+      title = f.readline()
+      titlePath = str.strip(i[0]+'/'+title, '\n')
+      f.close
+      path2title[filePath] = (title, titlePath)
+      title2path[titlePath] = (fileName, filePath)
+    # set key for folders
+    if len(i[2]):
+#      print("i[0]: %s/ i[2]: %s" % (i[0],i[2]))
+      path2title[i[0]+'/'] =  path2title[i[0]+'/'+i[2][0]]
+      path2title[i[0]] =  path2title[i[0]+'/'+i[2][0]]
+      title2path[i[0]+'/'] = title2path[path2title[i[0]+'/'+i[2][0]][1]]
+      title2path[i[0]] = title2path[path2title[i[0]+'/'+i[2][0]][1]]
+  return title2path, path2title
+
 @route('/wiki')
 @route('/wiki/')
 @route('/wiki/:page#.+#')
 @view('wiki_2col')
 def wiki(page=''):
-  fullpath = './wiki/%s' % page
-  if not os.path.isfile(fullpath):
-    if os.path.isdir(fullpath):
-      files = os.listdir(fullpath)
-      dirname = fullpath
-      if os.path.exists(fullpath+'index'): page = page + 'index'
-      else: page = page +'/'+ files[0]
-    else:
-      dirname = '/wiki'
-  else: 
-    files = os.listdir(os.path.dirname(fullpath))
+  # /wiki/machin/blah blah blah
+  # -> fullpath = /wiki/machin/blah.txt
+  # -> <a href=path2title['fullpath']['1']>title</a>
+  page, fullpath = title2path['wiki/%s' % page]
+  if os.path.isdir(fullpath):
+    files = os.listdir(fullpath)
+    dirname = fullpath
+  else:
     dirname = os.path.dirname(fullpath)
+    files = os.listdir(dirname)
+
   if not page: page = 'index' 
-  input_str = static_file(page, root='./wiki').output.read()
+  print("page: %s, fullpath: %s, dirname: %s" % (page, fullpath, dirname))
+  print(static_file(page, root=dirname).output)
+  input_str = static_file(page, root=dirname).output.read()
   try:
     import textile
   except Exception:
     input_str = input_str.decode().replace('\n','<br/>')
     return dict(page=page,content=input_str, files=files, path=dirname)
   else:
-    return dict(page=page+"_textile",content=render_textile(input_str), files=files, path=dirname)
+    return dict(page=page+"_textile",content=render_textile(input_str), files=files, path=dirname, t2p=title2path, p2t=path2title)
 
 @route('/static/:filename#.+#')
 def server_static(filename):
@@ -63,5 +91,11 @@ def error404(error):
 
 import bottle
 bottle.debug(True)
+global title2path
+global path2title
+(title2path, path2title) = wiki_url()
+print(path2title.keys())
+print(title2path.keys())
+print(path2title['wiki/linux-kernel/imac12.2-arch/linux-imac121.preset'])
 run(host='localhost', port=8080,reloader=True)
 #run(host='172.16.0.163', port=8080)
