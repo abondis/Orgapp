b166348b63036ac9f593c851ea2af4ce4f53c011
Aurélien Bondis <aurelien.bondis@gmail.com>
2012-08-01 22:26:38 -0400

    add basic wiki editing

--- app/webui.py
+++ app/webui.py
@@ -1,5 +1,9 @@
-from lib.bottle import route, run, static_file
-from lib.tasks import Orgapp
+import sys
+sys.path.extend(['lib'])
+from bottle import route, run, static_file, request
+from bottle import view
+from tasks import Orgapp
+import doc
 
 
 t = Orgapp()
@@ -7,9 +11,27 @@ t = Orgapp()
 def hello():
   return "Hello World!"
 
+
+@route('/doc/<path>/edit')
+@view('edit_wiki_page')
+def edit_wiki_page(path):
+  content = doc.render("../doc/{0}.md".format(path))
+  pagename = '/doc/'+path
+  return(dict(pagename=pagename, content=content))
+
+@route('/doc/<path>/edit', method='POST')
+@view('edit_wiki_page')
+def save_wiki_page(path):
+  content = request.forms.content
+  doc.save("../doc/{0}.md".format(path), content)
+  pagename = '/doc/'+path
+  return(dict(pagename=pagename, content=content))
+
+#TODO: cache pages another way, ie: cp -R doc cache + render
 @route('/doc/<filename:re:[^\.]*>')
 def show_wiki_page(filename):
   return static_file(filename, "../doc/cache") 
+  
 
 @route('/doc/<path:path>')
 def show_wiki_resources(path):
