d86a57200802b7b9fa445ef08d19f03ecc60966f
Aurélien Bondis <aurelien.bondis@gmail.com>
2012-11-12 17:05:48 -0500

    move code ui to codeui.py

---
+++ app/codeui.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python
+#-=- encoding: utf-8 -=-
+import sys
+sys.path.extend(['lib'])
+from webui import is_logged
+from bottle import request
+from bottle import view, redirect, url
+from bottle import post, get
+from tasks import Tasks
+from beaker.middleware import SessionMiddleware
+from cork import Cork
+import mercurial.commands as hg
+from mercurial import ui, localrepo
+from dulwich import repo
+from doc import Doc
+from tasks import Tasks
+
+
+t = Tasks()
+d = Doc()
+d.cache_all()
+hgui = ui.ui()
+auth = Cork('config')
+
+def make_code_menu(project, pagename=None):
+    """gets a list a menus for the wiki pages"""
+    menu = []
+    if pagename:
+        # display details
+        pass
+    menu.append(
+        {'url': url("show_tree", project=project), 'title': "Show tree"})
+    menu.append(
+        {'url': url('show_commits', project=project), 'title': "Show commits"})
+    return(menu)
+
+
+@get('/<project>/code/commits', name='show_commits')
+@view('show_list')
+def show_commits(project):
+    """Show repo's commits"""
+    s = request.environ.get('beaker.session')
+    if type(d.r[project]) == repo.Repo:
+        s['branch'] = request.query.branch or \
+            s.get('branch', 'HEAD')
+        # using walker and a ref (branch)
+        branches = d.r[project].get_refs()
+        if s['branch'] in branches:
+            w = d.r[project].get_walker([d.r[project].refs[s['branch']]])
+            #w = r.get_walker([r.refs['refs/heads/sqlite']])
+            l = [x.commit for x in w]
+        else:
+            l = ["Nothing has yet been done on your repo..."]
+    elif type(d.r[project]) == localrepo.localrepository:
+        s['branch'] = request.query.branch or \
+            s.get('branch', 'default')
+        branches = d.r[project].branchmap().keys()
+        #if s['branch'] not in branches:
+        try:
+            hgui.pushbuffer()
+            hg.log(hgui, d.r[project], branch=[s['branch']])
+            l = hgui.popbuffer().split('\n\n')
+        except:
+            s['branch'] = 'default'
+            l = ["Nothing has yet been done on your repo..."]
+    menu = make_code_menu(project)
+    return(
+        dict(
+            listing=l,
+            branches=branches,
+            leftmenu=menu,
+            project=project,
+            current_branch=s['branch'],
+            title="Show commits"))
+
+
+@get('/<project>/code/browse/<path:path>/show', name='display_file')
+@view('display_file')
+def display_file(path, project):
+    s = request.environ.get('beaker.session')
+    if type(d.r[project]) == repo.Repo:
+        s['branch'] = request.query.branch or \
+            s.get('branch', 'HEAD')
+        # get the tree for the branch
+        branches = d.r[project].get_refs()
+        tree_id = d.r[project][s['branch']].tree
+        # get the objects in this tree
+        objects = d.r[project].object_store.iter_tree_contents(tree_id)
+        path_sha = [x.sha for x in objects if x.path == path]
+        if len(path_sha) == 1:
+            content = d.r[project].get_object(path_sha[0])
+            content = content.as_raw_string().split('\n')
+        else:
+            content = ["sorry, could not find this file!"]
+    elif type(d.r[project]) == localrepo.localrepository:
+        s['branch'] = request.query.branch or \
+            s.get('branch', 'default')
+        branches = d.r[project].branchmap().keys()
+        if s['branch'] not in branches:
+            s['branch'] = d.r[project].branchmap().keys()[0]
+        _br = d.r[project][s['branch']]
+        hgui.pushbuffer()
+        hg.locate(hgui, d.r[project], branch=s['branch'])
+        _file = hgui.popbuffer().split('\n')
+        if path in _file:
+            content = _br.filectx(path).data().split('\n')
+        else:
+            content = ["sorry, could not find this file!"]
+    menu = make_code_menu(project)
+    return(
+        dict(
+            content=content,
+            project=project,
+            leftmenu=menu,
+            branches=branches,
+            current_branch=s['branch'],
+            title="Display " + path))
+
+
+@get('/<project>/code/browse', name='show_tree')
+@get('/<project>/code/browse/', name='show_tree')
+@get('/<project>/code/browse/<path:path>', name='show_tree')
+@view('show_files')
+def show_tree(project, path=''):
+    """Show repo's tree"""
+    if path == "":
+        current_path = '/' + project + '/code/browse'
+    else:
+        current_path = '/' + project + '/code/browse/' + path
+    s = request.environ.get('beaker.session')
+    l = []
+    if type(d.r[project]) == repo.Repo:
+        s['branch'] = request.query.branch or \
+            s.get('branch', 'HEAD')
+        # get the tree for the branch
+        branches = d.r[project].get_refs().keys()
+        if s['branch'] in branches:
+            tree_id = d.r[project][s['branch']].tree
+            # get the objects in this tree
+            objects = d.r[project].object_store.iter_tree_contents(tree_id)
+            #get the paths of the objects
+            l = [x.path for x in objects]
+        else:
+            l = ["Nothing has yet been done on your repo..."]
+    elif type(d.r[project]) == localrepo.localrepository:
+        s['branch'] = request.query.branch or \
+            s.get('branch', 'default')
+        branches = d.r[project].branchmap().keys()
+        print branches
+        #try:
+        if s['branch'] not in branches:
+            s['branch'] = d.r[project].branchmap().keys()[0]
+        hgui.pushbuffer()
+        l = hg.locate(hgui, d.r[project], branch=s['branch'])
+        l = hgui.popbuffer().split('\n')
+        print l
+        #except:
+            #s['branch'] = 'default'
+            #l = ["Nothing has yet been done on your repo..."]
+    dico = {}
+    for x in l:
+        l2w(x, dico)
+    hierarchy = dico[path]
+    menu = make_code_menu(project)
+    return(
+        dict(
+            current_path=current_path,
+            listing=hierarchy,
+            leftmenu=menu,
+            branches=branches,
+            project=project,
+            current_branch=s['branch'],
+            title="Show tree"))
--- app/webui.py
+++ app/webui.py
@@ -2,7 +2,6 @@
 import sys
 import os
 sys.path.extend(['lib'])
-import tasksui
 from bottle import run, static_file, request
 from bottle import view, redirect, template, url
 from bottle import post, get
@@ -12,9 +11,6 @@ from tasks import Tasks
 from doc import Doc
 from beaker.middleware import SessionMiddleware
 #from mercurial.hgweb import hgweb
-import mercurial.commands as hg
-from mercurial import ui, localrepo
-from dulwich import repo
 from cork import Cork
 from bottle import SimpleTemplate
 
@@ -22,7 +18,6 @@ from bottle import SimpleTemplate
 t = Tasks()
 d = Doc()
 d.cache_all()
-hgui = ui.ui()
 
 # Use users.json and roles.json in the local example_conf directory
 aaa = Cork('config')
@@ -77,155 +72,6 @@ def l2w(_d, dico, idx=1):
         l2w(_path[0], dico, idx=0)
 
 
-def make_code_menu(project, pagename=None):
-    """gets a list a menus for the wiki pages"""
-    menu = []
-    if pagename:
-        # display details
-        pass
-    menu.append(
-        {'url': url("show_tree", project=project), 'title': "Show tree"})
-    menu.append(
-        {'url': url('show_commits', project=project), 'title': "Show commits"})
-    return(menu)
-
-
-@get('/<project>/code/commits', name='show_commits')
-@view('show_list')
-def show_commits(project):
-    """Show repo's commits"""
-    s = request.environ.get('beaker.session')
-    if type(d.r[project]) == repo.Repo:
-        s['branch'] = request.query.branch or \
-            s.get('branch', 'HEAD')
-        # using walker and a ref (branch)
-        branches = d.r[project].get_refs()
-        if s['branch'] in branches:
-            w = d.r[project].get_walker([d.r[project].refs[s['branch']]])
-            #w = r.get_walker([r.refs['refs/heads/sqlite']])
-            l = [x.commit for x in w]
-        else:
-            l = ["Nothing has yet been done on your repo..."]
-    elif type(d.r[project]) == localrepo.localrepository:
-        s['branch'] = request.query.branch or \
-            s.get('branch', 'default')
-        branches = d.r[project].branchmap().keys()
-        #if s['branch'] not in branches:
-        try:
-            hgui.pushbuffer()
-            hg.log(hgui, d.r[project], branch=[s['branch']])
-            l = hgui.popbuffer().split('\n\n')
-        except:
-            s['branch'] = 'default'
-            l = ["Nothing has yet been done on your repo..."]
-    menu = make_code_menu(project)
-    return(
-        dict(
-            listing=l,
-            branches=branches,
-            leftmenu=menu,
-            project=project,
-            current_branch=s['branch'],
-            title="Show commits"))
-
-
-@get('/<project>/code/browse/<path:path>/show', name='display_file')
-@view('display_file')
-def display_file(path, project):
-    s = request.environ.get('beaker.session')
-    if type(d.r[project]) == repo.Repo:
-        s['branch'] = request.query.branch or \
-            s.get('branch', 'HEAD')
-        # get the tree for the branch
-        branches = d.r[project].get_refs()
-        tree_id = d.r[project][s['branch']].tree
-        # get the objects in this tree
-        objects = d.r[project].object_store.iter_tree_contents(tree_id)
-        path_sha = [x.sha for x in objects if x.path == path]
-        if len(path_sha) == 1:
-            content = d.r[project].get_object(path_sha[0])
-            content = content.as_raw_string().split('\n')
-        else:
-            content = ["sorry, could not find this file!"]
-    elif type(d.r[project]) == localrepo.localrepository:
-        s['branch'] = request.query.branch or \
-            s.get('branch', 'default')
-        branches = d.r[project].branchmap().keys()
-        if s['branch'] not in branches:
-            s['branch'] = d.r[project].branchmap().keys()[0]
-        _br = d.r[project][s['branch']]
-        hgui.pushbuffer()
-        hg.locate(hgui, d.r[project], branch=s['branch'])
-        _file = hgui.popbuffer().split('\n')
-        if path in _file:
-            content = _br.filectx(path).data().split('\n')
-        else:
-            content = ["sorry, could not find this file!"]
-    menu = make_code_menu(project)
-    return(
-        dict(
-            content=content,
-            project=project,
-            leftmenu=menu,
-            branches=branches,
-            current_branch=s['branch'],
-            title="Display " + path))
-
-
-@get('/<project>/code/browse', name='show_tree')
-@get('/<project>/code/browse/', name='show_tree')
-@get('/<project>/code/browse/<path:path>', name='show_tree')
-@view('show_files')
-def show_tree(project, path=''):
-    """Show repo's tree"""
-    if path == "":
-        current_path = '/' + project + '/code/browse'
-    else:
-        current_path = '/' + project + '/code/browse/' + path
-    s = request.environ.get('beaker.session')
-    l = []
-    if type(d.r[project]) == repo.Repo:
-        s['branch'] = request.query.branch or \
-            s.get('branch', 'HEAD')
-        # get the tree for the branch
-        branches = d.r[project].get_refs().keys()
-        if s['branch'] in branches:
-            tree_id = d.r[project][s['branch']].tree
-            # get the objects in this tree
-            objects = d.r[project].object_store.iter_tree_contents(tree_id)
-            #get the paths of the objects
-            l = [x.path for x in objects]
-        else:
-            l = ["Nothing has yet been done on your repo..."]
-    elif type(d.r[project]) == localrepo.localrepository:
-        s['branch'] = request.query.branch or \
-            s.get('branch', 'default')
-        branches = d.r[project].branchmap().keys()
-        print branches
-        #try:
-        if s['branch'] not in branches:
-            s['branch'] = d.r[project].branchmap().keys()[0]
-        hgui.pushbuffer()
-        l = hg.locate(hgui, d.r[project], branch=s['branch'])
-        l = hgui.popbuffer().split('\n')
-        print l
-        #except:
-            #s['branch'] = 'default'
-            #l = ["Nothing has yet been done on your repo..."]
-    dico = {}
-    for x in l:
-        l2w(x, dico)
-    hierarchy = dico[path]
-    menu = make_code_menu(project)
-    return(
-        dict(
-            current_path=current_path,
-            listing=hierarchy,
-            leftmenu=menu,
-            branches=branches,
-            project=project,
-            current_branch=s['branch'],
-            title="Show tree"))
 
 
 def make_wiki_menu(project, pagename=None):
@@ -372,6 +218,8 @@ def is_logged():
         return True
     except:
         return False
+import tasksui
+import codeui
 
 if __name__ == '__main__':
     SimpleTemplate.defaults["is_logged"] = is_logged
